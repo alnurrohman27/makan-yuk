e4fbbec0b35def12563c8dfa8cc676a675cefec9
alnurrohman27 <adian273645@gmail.com>
2017-04-01 03:29:51 +0700

    Initial Commit

---
+++ footer.html
@@ -0,0 +1,92 @@
+<footer>
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3  col-md-3 col-sm-4 col-xs-6">
+				<h3> Kontak </h3>
+				<ul>
+					<li class="supportLi">
+					<a class="inline" href="about-us.html">Tentang Kami</a>
+					<br>
+					<p>Institut Teknologi Sepuluh Nopember Surabaya</p>
+					<h4><a class="inline" href="callto:+12025550151"> <strong> <i class="fa fa-phone"> </i> +6281225395946 </strong> </a></h4>
+					<h4><a class="inline" href="mailto:help@yourweb.com"> <i class="fa fa-envelope-o"> </i> makanyuk@makanyuk.com</a></h4>
+
+					</li>
+				</ul>
+			</div>
+			<div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+				<h3> Makanan </h3>
+				<ul>
+					<li><a href="index.html#">
+					Makanan Besar
+					</a></li>
+					<li><a href="index.html#">
+					Dessert</a></li>
+					<li><a href="index.html#">
+					Kue Basah
+					</a></li>
+					<li><a href="index.html#">Kue Kering
+					</a></li>
+				</ul>
+			</div>
+			<div style="clear:both" class="hide visible-xs"></div>
+				<div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+				<h3> Minuman </h3>
+					<ul class="list-unstyled footer-nav">
+						<li><a href="index.html#">Minuman Dingin
+						</a></li>
+						<li><a href="index.html#"> Es Krim
+						</a></li>
+						<li><a href="index.html#"> Jus Buah
+						</a></li>
+						<li><a href="index.html#"> Tradisional </a></li>
+						<li><a href="index.html#">
+						Minuman Spesial
+						</a></li>
+					</ul>
+			</div>
+			<div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+			<h3> Restaurant </h3>
+			<ul>
+			<li><a href="account.html"> Fast Food </a></li>
+			<li><a href="my-address.html"> Menengah </a></li>
+			<li><a href="wishlist.html"> Mewah </a></li>
+			<li><a href="order-list.html"> Sederhana </a></li>
+			</ul>
+			</div>
+			<div style="clear:both" class="hide visible-xs"></div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+				<h3> Media Sosial </h3>
+					<ul class="social">
+						<li><a href="http://facebook.com/"> <i class="fa fa-facebook"> &nbsp; </i> </a></li>
+						<li><a href="http://twitter.com/"> <i class="fa fa-twitter"> &nbsp; </i> </a></li>
+						<li><a href="https://plus.google.com/"> <i class="fa fa-google-plus"> &nbsp; </i> </a></li>
+						<li><a href="http://youtube.com/"> <i class="fa fa-pinterest"> &nbsp; </i> </a></li>
+						<li><a href="http://youtube.com/"> <i class="fa fa-youtube"> &nbsp; </i> </a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+ 
+ 
+<div class="footer-bottom" style="background-color: #A13A3A">
+<div class="container">
+<p class="pull-left" style="color: white"> © makan YUK! 2015 </p>
+<div class="pull-right paymentMethodImg" style="margin-right: 30px"><img height="30" class="pull-right" style="visible: hidden;" src="./index_files/master_card.png" alt="img"> <img height="30" class="pull-right" src="./index_files/visa_card.png" alt="img"><img height="30" class="pull-right" src="./index_files/paypal.png" alt="img">
+<img height="30" class="pull-right" src="./index_files/google_wallet.png" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/logo-bni.jpg" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/mandiri-logo.jpg" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/bri-logo.jpg" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/logo-solaria.jpg" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/logo-kaskus.jpg" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/logo-bukalapak.png" alt="img">
+<img height="30" class="pull-right" src="./img/sponsor/logo-gojek.jpg" alt="img">
+</div>
+</div>
+</div>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,341 @@
+<!-- Header -->
+
+<body class="pace-done"><div class="pace  pace-inactive"><div class="pace-progress" data-progress-text="100%" data-progress="99" style="width: 100%;">
+  <div class="pace-progress-inner"></div>
+</div>
+<div class="pace-activity"></div></div>
+ 
+<div class="modal signUpContent fade" id="ModalLogin" tabindex="-1" role="dialog">
+<div class="modal-dialog ">
+<div class="modal-content">
+<div class="modal-header" style="background-color: #A13A3A">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true"> × </button>
+<h3 class="modal-title-site text-center"> Masuk makan YUK! </h3>
+</div>
+<div class="modal-body">
+<div class="form-group login-username">
+<div>
+<input name="log" id="login-user" class="form-control input" size="20" placeholder="Masukan nama" type="text">
+</div>
+</div>
+<div class="form-group login-password">
+<div>
+<input name="Password" id="login-password" class="form-control input" size="20" placeholder="Password" type="password">
+</div>
+</div>
+<div class="form-group">
+<div>
+
+</div>
+</div>
+<div>
+<div>
+<input name="submit" class="btn  btn-block btn-lg btn-primary" value="MASUK" type="submit" style="background-color: #A13A3A">
+</div>
+</div>
+ 
+</div>
+<div class="modal-footer">
+<p class="text-center"> Belum Punya? <a data-toggle="modal" data-dismiss="modal" href="index.html#ModalSignup"> <b> Daftar Disini </a> <br>
+<a href="forgot-password.html"> Lupa password? </a></p>
+</div>
+</div>
+ 
+</div>
+ 
+</div>
+ 
+ 
+<div class="modal signUpContent fade" id="ModalSignup" tabindex="-1" role="dialog">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header" style="background-color: #A13A3A">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true"> × </button>
+<h3 class="modal-title-site text-center"> DAFTAR</h3>
+</div>
+<div class="modal-body">
+<div class="form-group reg-username">
+<div>
+<input name="login" class="form-control input" size="20" placeholder="Masukan Nama" type="text">
+</div>
+</div>
+<div class="form-group reg-email">
+<div>
+<input name="reg" class="form-control input" size="20" placeholder="Masukan Email" type="text">
+</div>
+</div>
+<div class="form-group reg-password">
+<div>
+<input name="password" class="form-control input" size="20" placeholder="Password" type="password">
+</div>
+</div>
+<div class="form-group">
+<div>
+</div>
+</div>
+<div>
+<div>
+<input name="submit" class="btn  btn-block btn-lg btn-primary" value="DAFTAR" type="submit" style="background-color: #A13A3A">
+</div>
+</div>
+ 
+</div>
+<div class="modal-footer">
+<p class="text-center"> Sudah Punya? <a data-toggle="modal" data-dismiss="modal" href="index.html#ModalLogin">
+<b> Masuk Disini </a></p>
+</div>
+</div>
+ 
+</div>
+ 
+</div>
+ 
+<!-- Menu Bar --> 
+
+<div class="navbar navbar-tshop navbar-fixed-top megamenu" role="navigation" style="background-color: #5D1F1F">
+<div class="navbar-top" style="background-color: #A13A3A">
+<div class="container" style="background-color: #A13A3A">
+<div class="row">
+<div class="col-lg-6 col-sm-6 col-xs-6 col-md-6">
+<div class="pull-left ">
+
+</div>
+</div>
+<div class="col-lg-6 col-sm-6 col-xs-6 col-md-6 no-margin no-padding">
+<div class="pull-right">
+<ul class="userMenu">
+<li><a href="account-1.html"><span class="hidden-xs"> AKUN KU</span> <i class="glyphicon glyphicon-user hide visible-xs "></i></a></li>
+<li><a href="index.html#" data-toggle="modal" data-target="#ModalLogin"> <span class="hidden-xs">Masuk</span>
+<i class="glyphicon glyphicon-log-in hide visible-xs "></i> </a></li>
+<li class="hidden-xs"><a href="index.html#" data-toggle="modal" data-target="#ModalSignup"> Daftar</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+ 
+<div class="container" >
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="sr-only"> Toggle navigation </span> <span class="icon-bar"> </span> <span class="icon-bar"> </span> <span class="icon-bar"> </span></button>
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-cart"><i class="fa fa-shopping-cart colorWhite"> </i> <span class="cartRespons colorWhite"> Cart ($210.00) </span></button>
+<a class="navbar-brand " href="./index_files/index.html"> <img src="./img/logo.png" alt="makanYUK" width="54" height="35"> </a>
+ 
+
+</div>
+ 
+<div class="navbar-cart  collapse">
+<div class="cartMenu  col-lg-4 col-xs-12 col-md-4 ">
+<div class="w100 miniCartTable scroll-pane mCustomScrollbar _mCS_1" style="overflow: hidden;"><div class="mCustomScrollBox mCS-dark-2" id="mCSB_1" style="position: relative; height: 100%; overflow: hidden; max-width: 100%; max-height: 300px;"><div class="mCSB_container mCS_no_scrollbar" style="position:relative; top:0;">
+<table>
+<tbody>
+<tr class="miniCartProduct">
+<td style="width:20%" class="miniCartProductThumb">
+<div><a href="product-details.html"> <img src="/img/makanan/category/bakso/bakso-klenger-surabaya-1.jpg" alt="img"> </a>
+</div>
+</td>
+<td style="width:40%">
+<div class="miniCartDescription">
+<h4><a href="product-details.html"> Bakso Klenger Surabaya </a></h4>
+<div class="price"><span> Rp 50.000,00 </span></div>
+</div>
+</td>
+<td style="width:10%" class="miniCartQuantity"><a> X 1 </a></td>
+<td style="width:15%" class="miniCartSubtotal"><span> Rp 50.000,00 </span></td>
+<td style="width:5%" class="delete"><a> x </a></td>
+</tr>
+<tr class="miniCartProduct">
+<td style="width:20%" class="miniCartProductThumb">
+<div><a href="product-details.html"> <img src="./img/minuman/category/soda/float.jpg" alt="img"> </a>
+</div>
+</td>
+<td style="width:40%">
+<div class="miniCartDescription">
+<h4><a href="product-details.html"> KFC Float </a></h4>
+<div class="price"><span> Rp 6.500,00 </span></div>
+</div>
+</td>
+<td style="width:10%" class="miniCartQuantity"><a> X 1 </a></td>
+<td style="width:15%" class="miniCartSubtotal"><span> Rp 6.500,00 </span></td>
+<td style="width:5%" class="delete"><a> x </a></td>
+</tr>
+
+</tbody>
+</table>
+</div><div class="mCSB_scrollTools" style="position: absolute; display: none;"><div class="mCSB_draggerContainer"><div class="mCSB_dragger" style="position: absolute; top: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar" style="position:relative;"></div></div><div class="mCSB_draggerRail"></div></div></div></div></div>
+ 
+<div class="miniCartFooter  miniCartFooterInMobile text-right">
+<h3 class="text-right subtotal"> Total: Rp 56.500,00 </h3>
+<a class="btn btn-sm btn-danger" href="cart.html"> <i class="fa fa-shopping-cart"> </i> LIHAT KERANJANG
+</a> <a href="checkout-0.html" class="btn btn-sm btn-primary"> PERIKSA </a></div>
+ 
+</div>
+ 
+</div>
+ 
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav" >
+<li class="active" style="background-color: #bb4444"><a href="#"> Beranda </a></li>
+<li class="dropdown megamenu-fullwidth"><a data-toggle="dropdown" class="dropdown-toggle" href="#"> Update Terbaru<b class="caret"> </b> </a>
+<ul class="dropdown-menu">
+<li class="megamenu-content ">
+<ul class="col-lg-3  col-sm-3 col-md-3 unstyled noMarginLeft newCollectionUl">
+<li class="no-border">
+<p class="promo-1" style="background-color: #bb4444" ><strong> Pendatang Baru </strong></p>
+</li>
+<li><a href="category.html"> Semua Jenis </a></li>
+<li><a href="category.html"> Makanan </a></li>
+<li><a href="category.html"> Minuman </a></li>
+<li><a href="category.html"> Restaurant </a></li>
+<li><a href="category.html"> Kue </a></li>
+</ul>
+<ul class="col-lg-3  col-sm-3 col-md-3  col-xs-4">
+<li><a class="newProductMenuBlock" href="product-details.html"> <img class="img-responsive" width="300" height="250" src="./img/makanan/bakso.jpg" alt="product"> <span class="ProductMenuCaption"> <i class="fa fa-caret-right"> </i> MAKANAN</span>
+</a></li>
+</ul>
+<ul class="col-lg-3  col-sm-3 col-md-3 col-xs-4">
+<li><a class="newProductMenuBlock" href="product-details.html"> <img class="img-responsive" src="./img/minuman/soda.jpg" alt="product"> <span class="ProductMenuCaption"> <i class="fa fa-caret-right"> </i> MINUMAN </span> </a></li>
+</ul>
+<ul class="col-lg-3  col-sm-3 col-md-3 col-xs-4">
+<li><a class="newProductMenuBlock" href="product-details.html"> <img class="img-responsive" src="./img/restaurant/singapur.jpg" alt="product"> <span class="ProductMenuCaption"> <i class="fa fa-caret-right"> </i> RESTORAN </span>
+</a></li>
+</ul>
+</li>
+</ul>
+</li>
+ 
+<li class="dropdown megamenu-80width "><a data-toggle="dropdown" class="dropdown-toggle" href="index.html#"> Jualan
+<b class="caret"> </b> </a>
+<ul class="dropdown-menu">
+<li class="megamenu-content">
+ 
+<ul class="col-lg-2  col-sm-2 col-md-2  unstyled noMarginLeft">
+<li>
+<p><strong> Makanan </strong></p>
+</li>
+<li><a href="category.html#"> Bakso </a></li>
+<li><a href="category.html#"> Penyetan </a></li>
+<li><a href="category.html#"> Burger </a></li>
+<li><a href="category.html#"> Pizza </a></li>
+<li><a href="category.html#"> Kue Basah </a></li>
+<li><a href="category.html#"> Kue Kering </a></li>
+<li><a href="category.html#"> Tampilkan Semua </a></li>
+</ul>
+<ul class="col-lg-2  col-sm-2 col-md-2  unstyled">
+<li>
+<p><strong> Minuman </strong></p>
+</li>
+<li><a href="category.html#"> Jus Buah </a></li>
+<li><a href="category.html#"> Soda </a></li>
+<li><a href="category.html#"> Es Buah </a></li>
+<li><a href="category.html#"> Es Krim </a></li>
+<li><a href="category.html#"> Soft Drink </a></li>
+<li><a href="category.html#"> Minuman Hangat </a></li>
+<li><a href="category.html#"> Tampilkan Semua </a></li>
+</ul>
+<ul class="col-lg-2  col-sm-2 col-md-2  unstyled">
+<li>
+<p><strong> Restoran </strong></p>
+</li>
+<li><a href="category.html#"> Sederhana </a></li>
+<li><a href="category.html#"> Menengah </a></li>
+<li><a href="category.html#"> Atas </a></li>
+<li><a href="category.html#"> Fast Food </a></li>
+<li><a href="category.html#"> Ketering </a></li>
+<li><a href="category.html#"> Tampilkan Semua </a></li>
+</ul>
+<ul class="col-lg-3  col-sm-3 col-md-3 col-xs-6">
+<li class="no-margin productPopItem "><a href="product-details.html"> <img class="img-responsive" src="./img/makanan/pizza.png" alt="img"> </a> <a class="text-center productInfo alpha90" href="product-details.html"> Pizza<br>
+<span> mulai 12 ribu</span> </a></li>
+</ul>
+<ul class="col-lg-3  col-sm-3 col-md-3 col-xs-6">
+<li class="no-margin productPopItem relative"><a href="product-details.html"> <img class="img-responsive" src="./img/makanan/penyetan.jpg" alt="img"> </a> <a class="text-center productInfo alpha90" href="product-details.html"> Penyetan<br>
+<span> mulai 7 ribu </span> </a></li>
+</ul>
+
+
+<ul class="col-lg-3  col-sm-3 col-md-3 col-xs-6">
+<li class="no-margin productPopItem relative"><a href="product-details.html"> <img class="img-responsive" src="./img/makanan/burger.jpg" alt="img"> </a> <a class="text-center productInfo alpha90" href="product-details.html"> Burger<br>
+<span> mulai 8 ribu </span> </a></li>
+</ul>
+</li>
+</ul>
+</li>
+
+
+</ul>
+
+ 
+<div class="nav navbar-nav navbar-right hidden-xs">
+<div class="dropdown  cartMenu "><a href="index.html#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-shopping-cart"> </i> <span class="cartRespons"> Keranjang Belanja</span> <b class="caret"> </b> </a>
+<div class="dropdown-menu col-lg-4 col-xs-12 col-md-4 ">
+<div class="w100 miniCartTable scroll-pane mCustomScrollbar _mCS_2" style="overflow: hidden;"><div class="mCustomScrollBox mCS-dark-2" id="mCSB_2" style="position: relative; height: 100%; overflow: hidden; max-width: 100%; max-height: 300px;"><div class="mCSB_container mCS_no_scrollbar" style="position:relative; top:0;">
+<table>
+<tbody>
+<tr class="miniCartProduct">
+<td style="width:20%" class="miniCartProductThumb">
+<div><a href="product-details.html"> <img src="./img/makanan/category/bakso/bakso-klenger-surabaya-1.jpg" alt="img"> </a>
+</div>
+</td>
+<td style="width:40%">
+<div class="miniCartDescription">
+<h4><a href="product-details.html"> Bakso Klenger Surabaya </a></h4>
+<div class="price"><span> Rp 50.000,00 </span></div>
+</div>
+</td>
+<td style="width:10%" class="miniCartQuantity"><a> X 1 </a></td>
+<td style="width:15%" class="miniCartSubtotal"><span> Rp 50.000,00 </span></td>
+<td style="width:5%" class="delete"><a> x </a></td>
+</tr>
+<tr class="miniCartProduct">
+<td style="width:20%" class="miniCartProductThumb">
+<div><a href="product-details.html"> <img src="./img/minuman/category/soda/float.jpg" alt="img"> </a>
+</div>
+</td>
+<td style="width:40%">
+<div class="miniCartDescription">
+<h4><a href="product-details.html"> KFC Float </a></h4>
+<div class="price"><span> Rp 6.500,00 </span></div>
+</div>
+</td>
+<td style="width:10%" class="miniCartQuantity"><a> X 1 </a></td>
+<td style="width:15%" class="miniCartSubtotal"><span> Rp 6.500,00 </span></td>
+<td style="width:5%" class="delete"><a> x </a></td>
+</tr>
+
+</tbody>
+</table>
+</div><div class="mCSB_scrollTools" style="position: absolute; display: none;"><div class="mCSB_draggerContainer"><div class="mCSB_dragger" style="position: absolute; top: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar" style="position:relative;"></div></div><div class="mCSB_draggerRail"></div></div></div></div></div>
+ 
+<div class="miniCartFooter text-right">
+<h3 class="text-right subtotal"> Rp 56.500,00 </h3>
+<a class="btn btn-sm btn-danger" href="cart.html"> <i class="fa fa-shopping-cart"> </i> LIHAT KERANJANG
+ </a><a class="btn btn-sm btn-primary"> Periksa</a></div>
+ 
+</div>
+ 
+</div>
+ 
+<div class="search-box" >
+<div class="input-group">
+<button class="btn btn-nobg getFullSearch" type="button"><i class="fa fa-search"> </i></button>
+</div>
+ 
+</div>
+ 
+</div>
+ 
+</div>
+ 
+</div>
+ 
+<div class="search-full text-right" style="background-color: #c96969;"><a class="pull-right search-close" > <i class=" fa fa-times-circle" style="color: white;"> </i> </a>
+<div class="searchInputBox pull-right" style="background-color: #c96969;">
+<input type="search" data-searchurl="search?=" name="q" placeholder="Ketik kata kunci" class="search-input" style="background-color: #c96969;">
+<button class="btn-nobg search-btn" type="submit" style="background-color: #c96969;"><i class="fa fa-search"> </i></button>
+</div>
+</div>
+ 
+</div>
+
+<!-- Akhir Header -->
